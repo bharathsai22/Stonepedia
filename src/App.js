11be4ef7bc7682f47d6ee068b07a8eb0e8162fa0
bharathsai22 <bharathsaigoli@gmail.com>
2024-12-13 16:51:16 +0530

    footer section modified

--- src/App.js
+++ src/App.js
@@ -160,136 +160,13 @@ function App() {
             </div>
         </div>
     </div>
-    <div class="healthy-food-section pt-5 pb-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-5">
-                    <div class="text-center">
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/healthy-food-plate-img.png" class="healthy-food-section-img" />
-                    </div>
-                </div>
-                <div class="col-12 col-md-7">
-                    <h1 class="healthy-food-section-heading">
-                        Fresh, Healthy, Organic, Delicious Fruits
-                    </h1>
-                    <p class="healthy-food-section-description">
-                        Say no to harmful chemicals and go fully organic with our range of
-                        fresh fruits and veggies. Pamper your body and your senses with
-                        the true and unadulterated gifts from mother nature. with the true
-                        and unadulterated gifts from mother nature.
-                    </p>
-                    <button class="custom-button">Watch Video</button>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="delivery-and-payment-section pt-5 pb-5" id="deliveryPaymentSection">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-5 order-1 order-md-2">
-                    <div class="text-center">
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/delivery-payment-section-img.png" class="delivery-and-payment-section-img" />
-                    </div>
-                </div>
-                <div class="col-12 col-md-7 order-2 order-md-1">
-                    <h1 class="delivery-and-payment-section-heading">
-                        Delivery and Payment
-                    </h1>
-                    <p class="delivery-and-payment-section-description">
-                        Enjoy hassle-free payment with the plenitude of payment options
-                        available for you. Get live tracking and locate your food on a
-                        live map. It's quite a sight to see your food arrive to your door.
-                        Plus, you get a 5% discount on every order every time you pay
-                        online.
-                    </p>
-                    <button class="custom-button">Order Now</button>
-                    <div class="mt-3">
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/visa-card-img.png" class="payment-card-img" />
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/master-card-img.png" class="payment-card-img" />
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/paypal-card-img.png" class="payment-card-img" />
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/american-express-img.png" class="payment-card-img" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="thanking-customers-section pt-5 pb-5">
-        <div class="container">
-            <div class="row">
-                <div class="col-12 col-md-7 d-flex flex-column justify-content-center">
-                    <h1 class="thanking-customers-section-heading">
-                        Thank you for being a valuable customer to us.
-                    </h1>
-                    <p class="thanking-customers-section-description">
-                        We have a surprise gift for you
-                    </p>
-                    <div class="d-md-none">
-                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/thanking-customers-section-img.png" class="thanking-customers-section-img" />
-                    </div>
-                    <div>
-                        <button type="button" class="custom-button" data-toggle="modal" data-target="#exampleModal">
-                            Redeem Gift
-                        </button>
-                        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog mt-5">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title thanking-customers-section-modal-title" id="exampleModalLabel">
-                                            Gift Voucher
-                                        </h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <span aria-hidden="true">&times;</span>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body">
-                                        <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/gift-voucher-img.png" class="w-100" />
-                                    </div>
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">
-                                            Close
-                                        </button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-5 d-none d-md-block">
-                    <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/thanking-customers-section-img.png" class="thanking-customers-section-img" />
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="follow-us-section pt-5 pb-5" id="followUsSection">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="follow-us-section-heading">Follow Us</h1>
-                </div>
-                <div class="col-12">
-                    <div class="d-flex flex-row justify-content-center">
-                        <div class="follow-us-icon-container">
-                            <i class="fab fa-twitter icon"></i>
-                        </div>
-                        <div class="follow-us-icon-container">
-                            <i class="fab fa-instagram icon"></i>
-                        </div>
-                        <div class="follow-us-icon-container">
-                            <i class="fab fa-facebook icon"></i>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="footer-section pt-5 pb-5">
         <div class="container">
             <div class="row">
                 <div class="col-12 text-center">
-                    <img src="https://d1tgh8fmlzexmh.cloudfront.net/ccbp-responsive-website/food-munch-logo-light.png" class="food-munch-logo" />
-                    <h1 class="footer-section-mail-id">orderfood@foodmunch.com</h1>
+                    
                     <p class="footer-section-address">
-                        123 Ayur Vigyan Nagar, New Delhi, India.
+                    Copyright © 2024 - StonePedia Private Limited. All Rights Reserved.
                     </p>
                 </div>
             </div>
